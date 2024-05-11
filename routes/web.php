a59e44bff15b3cdbd4a2b746d0ee1f9bc779be11
Jan <148061512+Papssz@users.noreply.github.com>
2024-05-11 20:02:33 +0800

    feat: Added new routes and fix structure

--- routes/web.php
+++ routes/web.php
@@ -9,25 +9,26 @@ use App\Http\Controllers\LeaveController;
 use App\Http\Controllers\PayrollController;
 use App\Http\Controllers\SignatoryController;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+// Mainmenu Route
+Route::get('/', [EmployeeController::class, 'index'])->name('employees.index');
 
 // Employees routes
-Route::prefix('employees')->group(function () {
-    Route::get('/details', [EmployeeController::class, 'showDetails'])->name('employees.details');
-    Route::get('/', [EmployeeController::class, 'index'])->name('employees.index');
-    Route::get('/create', [EmployeeController::class, 'create'])->name('employees.create');
-    Route::post('/', [EmployeeController::class, 'store'])->name('employees.store');
-    Route::get('/{employee}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
-    Route::put('/{employee}', [EmployeeController::class, 'update'])->name('employees.update');
-    Route::delete('/{employee}', [EmployeeController::class, 'destroy'])->name('employees.destroy');
-    Route::get('/{emp_num}/assign-designation', [AssignDesignationController::class, 'create'])
-        ->name('employees.assign-designation.create');
-    Route::post('/{employee_id}/assign-designations', [AssignDesignationController::class, 'store'])
-        ->name('assign-designations.store');
-    Route::resource('employees', EmployeeController::class);
-});
+Route::get('employees/details', [EmployeeController::class, 'showDetails'])->name('employees.details');
+Route::get('employees/create', [EmployeeController::class, 'create'])->name('employees.create');
+Route::post('employees', [EmployeeController::class, 'store'])->name('employees.store');
+Route::get('employees/{employee}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
+Route::put('employees/{employee}', [EmployeeController::class, 'update'])->name('employees.update');
+Route::delete('employees/{employee}', [EmployeeController::class, 'destroy'])->name('employees.destroy');
+Route::get('employees/{emp_num}/assign-designation', [AssignDesignationController::class, 'create'])
+    ->name('employees.assign-designation.create');
+Route::post('employees/{employee_id}/assign-designations', [AssignDesignationController::class, 'store'])
+    ->name('assign-designations.store');
+Route::resource('employees', EmployeeController::class)->except(['create', 'store', 'edit', 'update', 'destroy']);
+
 
 // Designations routes
 Route::resource('designations', DesignationController::class);
@@ -36,19 +37,17 @@ Route::resource('designations', DesignationController::class);
 Route::resource('departments', DepartmentController::class);
 
 // Leaves routes
-Route::prefix('leaves')->group(function () {
-    Route::get('/create', [LeaveController::class, 'create'])->name('leaves.create');
-    Route::post('/', [LeaveController::class, 'store'])->name('leaves.store');
-    Route::get('/', [LeaveController::class, 'index'])->name('leaves.index');
-});
+Route::get('leaves/create', [LeaveController::class, 'create'])->name('leaves.create');
+Route::post('leaves', [LeaveController::class, 'store'])->name('leaves.store');
+Route::get('leaves', [LeaveController::class, 'index'])->name('leaves.index');
+Route::delete('leaves/{leave}', [LeaveController::class, 'destroy'])->name('leaves.destroy');
 
 // Signatories routes
-Route::prefix('signatories')->group(function () {
-    Route::get('/', [SignatoryController::class, 'index'])->name('signatories.index');
-    Route::get('/create', [SignatoryController::class, 'create'])->name('signatories.create');
-    Route::post('/', [SignatoryController::class, 'store'])->name('signatories.store');
-});
+Route::get('signatories', [SignatoryController::class, 'index'])->name('signatories.index');
+Route::get('signatories/create', [SignatoryController::class, 'create'])->name('signatories.create');
+Route::post('signatories', [SignatoryController::class, 'store'])->name('signatories.store');
+Route::delete('signatories/{signatory}', [SignatoryController::class, 'destroy'])->name('signatories.destroy');
 
-
+// Payroll Routes
 Route::get('/payroll', [PayrollController::class, 'index'])->name('payroll.index');
 Route::post('/payroll/generate', [PayrollController::class, 'generatePayroll'])->name('payroll.generate');
