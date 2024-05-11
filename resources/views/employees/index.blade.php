746cd37cc182283d5a81f10b623c9de26fbae189
Jan <148061512+Papssz@users.noreply.github.com>
2024-05-11 20:02:16 +0800

    fix code structure of the navbar of the following blades

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -90,27 +90,13 @@
             <a href="#" class="text-2xl font-bold">Employee Management</a>
         </div>
         <ul class="space-y-4">
-        <li>
-            <a href="{{ route('employees.create') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Add New Employee</a>
-        </li>
-        <li>
-            <a href="{{ route('employees.details') }}" class="block hover:bg-black text-white font-bold py-2 px-2">View Employee</a>
-        </li>
-        <li>
-            <a href="{{ route('leaves.create') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Employee Leave Request</a>
-        </li>
-        <li>
-            <a href="{{ route('leaves.index') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Employee Leave Status</a>
-        </li>
-        <li>
-            <a href="{{ route('signatories.create') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Add Signatories</a>
-        </li>
-        <li>
-            <a href="{{ route('signatories.index') }}" class="block hover:bg-black text-white font-bold py-2 px-2">View Signatories</a>
-        </li>
-        <li>
-            <a href="#" class="block hover:bg-black text-white font-bold py-2 px-2">Employee Payroll</a>
-        </li>
+        <li><a href="{{ route('employees.create') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Add New Employee</a></li>
+        <li><a href="{{ route('employees.details') }}" class="block hover:bg-black text-white font-bold py-2 px-2">View Employee</a></li>
+        <li><a href="{{ route('leaves.create') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Employee Leave Request</a></li>
+        <li><a href="{{ route('leaves.index') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Employee Leave Status</a></li>
+        <li><a href="{{ route('signatories.create') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Add Signatories</a></li>
+        <li><a href="{{ route('signatories.index') }}" class="block hover:bg-black text-white font-bold py-2 px-2">View Signatories</a></li>
+        <li><a href="{{ route('payroll.index') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Employee Payroll</a></li>
     </ul>
     </nav>
 
@@ -137,7 +123,7 @@
                                             <div class=>
                                                 <p class="text-left mb-2 font-bold">{{ $employee->firstname }} {{ $employee->middlename }} {{ $employee->lastname }}</p>
                                                 <p class="text-left mb-2">ID: {{ $employee->id }}</p>
-                                                <p class="text-left mb-2">Employment Number: {{ $employee->emp_num }}</p>
+                                                <p class="text-left mb-2">Employee Number: {{ $employee->emp_num }}</p>
                                                 <p class="text-left mb-2">Address Line: {{ $employee->address_line }}</p>
                                                 <p class="text-left mb-2">Barangay: {{ $employee->brgy }}</p>
                                                 <p class="text-left mb-2">Province: {{ $employee->province }}</p>
--- resources/views/leaves/create_leave.blade.php
+++ resources/views/leaves/create_leave.blade.php
@@ -139,33 +139,19 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100 p-6">
-<nav class="bg-gray-800 text-white w-64 py-6 px-4 fixed top-0 left-0 h-full">
+    <nav class="bg-gray-800 text-white w-64 py-6 px-4 fixed top-0 left-0 h-full">
         <div class="flex items-center mb-8">
             <a href="#" class="text-2xl font-bold">Employee Management</a>
         </div>
         <ul class="space-y-4">
-            <li>
-                <a href="{{ route('employees.create') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Add New Employee</a>
-            </li>
-            <li>
-                <a href="{{ route('employees.details') }}" class="block hover:bg-black text-white font-bold py-2 px-2">View Employee</a>
-            </li>
-            <li>
-                <a href="{{ route('leaves.create') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Employee Leave Request</a>
-            </li>
-            <li>
-                <a href="{{ route('leaves.index') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Employee Leave Status</a>
-            </li>
-            <li>
-                <a href="{{ route('signatories.create') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Add Signatories</a>
-            </li>
-            <li>
-                <a href="{{ route('signatories.index') }}" class="block hover:bg-black text-white font-bold py-2 px-2">View Signatories</a>
-            </li>
-            <li>
-                <a href="{{ route('leaves.index') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Employee Payroll</a>
-            </li>
-        </ul>
+        <li><a href="{{ route('employees.create') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Add New Employee</a></li>
+        <li><a href="{{ route('employees.details') }}" class="block hover:bg-black text-white font-bold py-2 px-2">View Employee</a></li>
+        <li><a href="{{ route('leaves.create') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Employee Leave Request</a></li>
+        <li><a href="{{ route('leaves.index') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Employee Leave Status</a></li>
+        <li><a href="{{ route('signatories.create') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Add Signatories</a></li>
+        <li><a href="{{ route('signatories.index') }}" class="block hover:bg-black text-white font-bold py-2 px-2">View Signatories</a></li>
+        <li><a href="{{ route('payroll.index') }}" class="block hover:bg-black text-white font-bold py-2 px-2">Employee Payroll</a></li>
+    </ul>
     </nav>   
     <!-- Canvas -->
     <div class="flex bg-[#f0f2f5] justify-center">
